3100dccd963fce87f6259bb32ee02d32509c32cc
JoyRume <joy.rume@student.moringaschool.com>
2023-12-23 14:30:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // code your solution here
+
+ function saturdayFun(activity="roller-skate"){
+  if (activity){
+    return `This Saturday, I want to ${activity}!`;
+  }else {
+    console.log("This Saturday, I want to bathe my dog!")
+  }
+  
+ }
+ console.log(saturdayFun())
+
+ function mondayWork(activity="go to the office") {
+  if (activity) {
+      return `This Monday, I will ${activity}.`;
+  } else {
+     return 'This Monday, I will go to the office.';
+  }
+}
+ //const mondayWork= function(activity="go to the office"){
+  //if (activity){
+  // return `This Monday, I will ${activity}!`
+
+  //}else{
+    //console.log("This Monday, I will work from home.")
+
+  //}
+ //}
+
+ console.log(mondayWork())
+
+
+ let wrapAdjective = function(sign="*") {
+  return function(adjective="special") {
+      return `You are ${sign}${adjective}${sign}!`;
+  }
+}
+ //function wrapAdjective (string="*"){
+   //inner function(single="special"){
+     //   console.log(`You are ${string}${single}${string}`)
+
+    //}
+    //return inner
+ //}
+ const myInnerFunction = wrapAdjective(); 
+myInnerFunction()
+
+
+
+
+
